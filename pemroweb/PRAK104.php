cf5979f9ffe17e92fe0ee93fc201fed993d03275
MUHAMMAD ASANDY PUTRA <104177711+MuhammadAsandyPutra@users.noreply.github.com>
2023-03-15 22:14:02 +0800

    Update PRAK104.php

--- pemroweb/PRAK104.php
+++ pemroweb/PRAK104.php
@@ -10,7 +10,7 @@ $Seri = array("Samsung Galaxy S22", "Samsung Galaxy S22+", "Samsung Galaxy A03",
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>                     /*stytling yang digunakan pada tabel agar terlihat seperti pada contoh */
+    <style>                     /*styling yang digunakan pada tabel agar terlihat seperti pada contoh */
         table,td {
             border:1px solid;
         }
