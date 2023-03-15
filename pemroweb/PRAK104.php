8f5d80eca47ac9c75625433307a8ae1d755bf508
MUHAMMAD ASANDY PUTRA <104177711+MuhammadAsandyPutra@users.noreply.github.com>
2023-03-15 22:10:18 +0800

    Update PRAK104.php

--- pemroweb/PRAK104.php
+++ pemroweb/PRAK104.php
@@ -1,16 +1,16 @@
 <?php 
 $Seri = array("Samsung Galaxy S22", "Samsung Galaxy S22+", "Samsung Galaxy A03", "Samsung Xcover 5");
-
+//indexed array disini digunakan sebagai isi dari data yang ada pada tabel
 ?>
 
-<!DOCTYPE html>
+<!DOCTYPE html> //tag awal untuk html
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
+    <style> //stytling yang digunakan pada tabel agar terlihat seperti pada contoh
         table,td {
             border:1px solid;
         }
@@ -27,7 +27,7 @@ $Seri = array("Samsung Galaxy S22", "Samsung Galaxy S22+", "Samsung Galaxy A03",
         <th><?php echo "Daftar Smartphone Samsung"; ?></th>    
     </tr>
     <tr>
-        <td><?php echo $Seri[0]; ?></td>    
+        <td><?php echo $Seri[0]; ?></td>   //proses pemanggilan dan mencetak nilai array pada tabel yang telah dibuat
     </tr>
     <tr>
         <td><?php echo $Seri[1]; ?></td>    
