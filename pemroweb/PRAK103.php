9cb63b611471816bc0d29fa791a6ca5792a8d469
MUHAMMAD ASANDY PUTRA <104177711+MuhammadAsandyPutra@users.noreply.github.com>
2023-03-15 22:16:16 +0800

    Update PRAK103.php

--- pemroweb/PRAK103.php
+++ pemroweb/PRAK103.php
@@ -1,5 +1,5 @@
 <?php 
-$clc_float = 37.841;
+$clc_float = 37.841; /*Pembuatan variabel serta perhitungan rumus dengan nilainya masing-masing */
 $fahren_float = (9/5)* $clc_float+32;
 $ream_float = (4/5)* $clc_float;
 $kelv = $clc_float+273;
@@ -7,6 +7,7 @@ $kelv = $clc_float+273;
 echo "Celcius = ". $clc_float;
 echo "<br>===========Program Konversi Suhu==============<br>";
 
+/*Proses mencetak hasil dari konversi suhu*/
 echo "Fahrenheit (F) = ".$fahren_float. "<br>";
 echo "Reamur (R) = ".$ream_float. "<br>";
 echo "Kelvin (K) = ". $kelv;
