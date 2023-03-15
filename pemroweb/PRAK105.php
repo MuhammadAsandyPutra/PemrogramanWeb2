cc40b4182b12761a761df9a4e6b0b3b1bb53a2d8
MUHAMMAD ASANDY PUTRA <104177711+MuhammadAsandyPutra@users.noreply.github.com>
2023-03-15 22:06:38 +0800

    Update PRAK105.php

--- pemroweb/PRAK105.php
+++ pemroweb/PRAK105.php
@@ -1,18 +1,18 @@
 <?php 
-$Series[1] = "Samsung Galaxy S22";
+$Series[1] = "Samsung Galaxy S22"; //proses pembuatan array
 $Series[2] = "Samsung Galaxy S22+";
 $Series[3] = "Samsung Galaxy A03";
 $Series[4] = "Samsung Xover 5";
 ?>
 
-<!DOCTYPE html>
+<!DOCTYPE html> //tag awal untuk html
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
+    <style> //styling yang digunakan agar output seperti pada soal
         table,td {
             border:1px solid;
         }
@@ -30,7 +30,7 @@ $Series[4] = "Samsung Xover 5";
         <th>Daftar Smartphone Samsung</td>    
     </tr>
     <tr>
-        <td><?= $Series[1]; ?></td>    
+        <td><?= $Series[1]; ?></td>  //proses pemanggilan array kedalam tabel yang dibuat  
     </tr>
     <tr>
         <td><?= $Series[2]; ?></td>    
