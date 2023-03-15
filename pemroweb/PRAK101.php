27a4c349d7e947a8a0dbfbe7b221f2e344011fc1
MUHAMMAD ASANDY PUTRA <104177711+MuhammadAsandyPutra@users.noreply.github.com>
2023-03-15 22:17:10 +0800

    Update PRAK101.php

--- pemroweb/PRAK101.php
+++ pemroweb/PRAK101.php
@@ -1,5 +1,5 @@
 <?php 
 echo  "Hello World in PHP<br>";  //perintah echo berfungsi untuk menampilkan output, serta <br> untuk membuat baris baru
-echo  "Nama Praktikum : {Muhammad Asandy Putra}<br>";
+echo  "Nama Praktikan : Muhammad Asandy Putra<br>";
 echo  "NIM : 2110817310002";
 ?>
