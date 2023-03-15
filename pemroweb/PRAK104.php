e33c91f1ac0c64634053538dda6685e6c52dc7f2
MUHAMMAD ASANDY PUTRA <104177711+MuhammadAsandyPutra@users.noreply.github.com>
2023-03-15 22:07:36 +0800

    Add files via upload

---
+++ pemroweb/PRAK104.php
@@ -0,0 +1,44 @@
+<?php 
+$Seri = array("Samsung Galaxy S22", "Samsung Galaxy S22+", "Samsung Galaxy A03", "Samsung Xcover 5");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table,td {
+            border:1px solid;
+        }
+        th{
+            font: bold;
+            border: 1px solid;
+        }
+
+    </style>
+</head>
+<body>
+<table>
+    <tr>
+        <th><?php echo "Daftar Smartphone Samsung"; ?></th>    
+    </tr>
+    <tr>
+        <td><?php echo $Seri[0]; ?></td>    
+    </tr>
+    <tr>
+        <td><?php echo $Seri[1]; ?></td>    
+    </tr>
+    <tr>
+        <td><?php echo $Seri[2]; ?></td>    
+    </tr>
+    <tr>
+        <td><?php echo $Seri[3]; ?></td>    
+    </tr>
+</table>
+</body>
+</html>
+
